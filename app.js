df9f56ca41d238445ddbdf36d91815082a5184e6
ajayvarshaan <varshaanajay@gmail.com>
2025-07-14 13:48:11 +0530

    initial commit: basic express app setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/",(req,res,next) => {
+    res.send("Welcome devops")
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running sucessfully on te port ${PORT}`);
+    
+})
